e8ba4e658ce7542ace805da0d340c7a986e816a5
Kushal Patel <kushal300603@gmail.com>
2024-06-18 00:20:08 +0530

    fix: Bug at Deploying pg-table

--- backend/src/db.js
+++ backend/src/db.js
@@ -9,9 +9,13 @@ const client = new Client({
     port: process.env.POSTGRES_PORT,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DATABASE_NAME,
+    ssl: {
+        rejectUnauthorized: false // Note: Setting this to false can create security vulnerabilities in production
+    }
 });
 
-client.connect();
+client.connect()
+.then(() => console.log('Connected to the database'))
+.catch((err) => console.error('Connection error', err.stack));
 
 export default client;
-
